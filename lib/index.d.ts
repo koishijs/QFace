007fb710f8bbf3c627033d0a4d06901df7f21b4f
Shigma <1700011071@pku.edu.cn>
2021-05-16 20:36:16 +0800

    support get

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,1 +1,13 @@
+export interface Face {
+  QSid: string
+  QDes: string
+  IQLid: string
+  AQLid: string
+  EMCode: string
+  isStatic?: '1'
+  isCMEmoji?: '1'
+}
+
+export function get(id: string | number): Face
+
 export function getUrl(id: string | number, base?: string): string
