d7572bd3dc0b800f29acbea35377aa27c907b4fb
Dragon-Fish <xiaoyujun@aojiaogame.com>
2024-10-17 15:17:31 +0800

    feat: + qqnt 9.9.15-28498

--- public/index.js
+++ public/index.js
@@ -4,16 +4,23 @@
   /** @type {HTMLElement} */
   const placeholder = document.getElementById('placeholder')
   /** @type {import('../lib').Face[]} */
-  const list = await fetch('./lib/data.json').then((res) => res.json())
+  const config = await fetch('./assets/default_config.json').then((res) =>
+    res.json()
+  )
 
   placeholder.style.display = 'none'
 
+  const list = config.normalPanelResult.SysEmojiGroupList.map(
+    (l) => l.SysEmojiList
+  ).flat()
   list
-    .sort((a, b) => Number(a.QSid) - Number(b.QSid))
+    .sort((a, b) => Number(a.emojiId) - Number(b.emojiId))
     .forEach((item) => {
-      const { QSid, QDes, isStatic } = item
+      const { emojiId, describe = '', isStatic } = item
 
-      const src = isStatic ? `static/s${QSid}.png` : `gif/s${QSid}.gif`
+      const src = isStatic
+        ? `assets/qq_emoji/qq_emoji_${emojiId}.png`
+        : `assets/resfile/emoji/${emojiId}/apng/${emojiId}.png`
       const faceImg = document.createElement('img')
       faceImg.src = src
       faceImg.className = 'face-img'
@@ -22,16 +29,17 @@
         const div = document.createElement('div')
         div.className = 'face-img'
         div.textContent = '?'
+        div.dataset.src = src
         this.replaceWith(div)
       }
 
       const faceId = document.createElement('div')
       faceId.className = 'face-id'
-      faceId.textContent = QSid
+      faceId.textContent = emojiId
 
       const faceDesc = document.createElement('div')
       faceDesc.className = 'face-desc'
-      faceDesc.textContent = QDes.replace(/^\//, '')
+      faceDesc.textContent = describe.replace(/^\//, '')
 
       const link = document.createElement('a')
       link.href = src
@@ -39,10 +47,12 @@
       link.className = 'face'
       link.append(faceImg, faceDesc, faceId)
 
-      Object.entries(item, ([key, val]) => {
-        link.setAttribute(`data-${key}`, val)
-      })
+      // Object.entries(item, ([key, val]) => {
+      //   link.setAttribute(`data-${key}`, val)
+      // })
+      link.dataset.metadata = JSON.stringify(item)
 
       container.append(link)
     })
+  console.info('Loaded', list.length, 'faces')
 })()
